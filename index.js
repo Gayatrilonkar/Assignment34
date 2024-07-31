1454d0137c908d53ab555cf5ecc1a373b9f0a735
Gayarilonkar <gayatrilonkar048@gmail.com>
2024-07-31 23:54:47 +0530

    lagest number print

--- index.js
+++ index.js
@@ -14,3 +14,14 @@ for (const stud of students)
 
 
 console.log(`Hello ${stud}`)
+const marks = [ 100,200,300,10]
+
+
+let largest =marks[0]
+for (const num of marks){
+    if (num>largest){
+        largest=num
+
+    }
+}
+console.log(largest)
